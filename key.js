0128889c09e6dbc9c8ba82426dd435a430370f9d
TheMelonOne <paichun.wang@yahoo.com>
2018-08-11 10:55:39 -0500

    Added Twiiter function and key borrowed from JW

--- key.js
+++ key.js
@@ -1,4 +1,5 @@
 console.log("Loaded Key.js");
+require('dotenv').config({ path: './.env' })
 
 exports.twitter = {
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
--- liri.js
+++ liri.js
@@ -1,10 +1,13 @@
 //Requests and config files
 require('dotenv').config({ path: './.env' })
 
-let request = require("request");
+let request = require("request"); //Request System for exporting and importing functions/keys
 let Spotify = require('node-spotify-api');
-let dotenv = require('dotenv');
-let fs = require('fs');
+let dotenv = require('dotenv'); //Request System for custom enviroment
+let fs = require('fs'); //File System NPM for reading/writing files
+let Twitter = require('twitter');
+let keys = require('./key.js');
+
 
 //Variable declaration
 let action = process.argv[2]; // this is pre-defined since user will always have to enter a action, will prompt if user didn't eneter anything
@@ -19,6 +22,9 @@ else if (action == "spotify-this-song") { // Spotify
     if (variable == undefined) { variable = 'Ace of Base' } else { variable = process.argv[3]; }
     spotify(variable);
 }// Spotify
+else if (action == "my-tweets") {
+    twitter();
+}
 else if (action == "do-what-it-says") {
     random();
 }
@@ -35,27 +41,26 @@ function movie(variable) {
     request(`http://www.omdbapi.com/?t=${variable}&y=&plot=short&apikey=trilogy`, function (error, response, body) {
         // console.log("API RAN"); // Checking if IMDB API ran
         if (!error && response.statusCode === 200) {
+            // console.log('\n' +
+            //     "Movie Title: ".padEnd(28) + JSON.parse(body).Title + '\n' +
+            //     "Movie Release Year: ".padEnd(28) + JSON.parse(body).Year + '\n' +
+            //     "IMDB Rating: ".padEnd(28) + JSON.parse(body).Rated + '\n' +
+            //     // "Rotten Tomatoes Rating: ".padEnd(28) + JSON.parse(body).Ratings[0].Value + '\n' +
+            //     "Country of Production: ".padEnd(28) + JSON.parse(body).Country + '\n' +
+            //     "Language Dialect: ".padEnd(28) + JSON.parse(body).Language + '\n' +
+            //     "Movie Plot: ".padEnd(28) + JSON.stringify(JSON.parse(body).Plot) + '\r\n' +
+            //     "Movie Actors: ".padEnd(28) + JSON.parse(body).Actors + '\n'
+            // );
+
+            // console.log(`\n "Movie Title: "${.padend(28)}`)
         }
-        console.log('\n' +
-            "Movie Title: ".padEnd(28) + JSON.parse(body).Title + '\n' +
-            "Movie Release Year: ".padEnd(28) + JSON.parse(body).Year + '\n' +
-            "IMDB Rating: ".padEnd(28) + JSON.parse(body).Rated + '\n' +
-            "Rotten Tomatoes Rating: ".padEnd(28) + JSON.parse(body).Ratings[1].Value + '\n' +
-            "Country of Production: ".padEnd(28) + JSON.parse(body).Country + '\n' +
-            "Language Dialect: ".padEnd(28) + JSON.parse(body).Language + '\n' +
-            "Movie Plot: ".padEnd(28) + JSON.stringify(JSON.parse(body).Plot) + '\r\n' +
-            "Movie Actors: ".padEnd(28) + JSON.parse(body).Actors + '\n'
-        );
     });
 }
 
 function spotify(variable) { //This code was supplied by node-spotify-api
 
-    // console.log(process.env.SPOTIFY_ID);
-    // console.log(process.env.SPOTIFY_SECRET);
-
-    let myid = process.env.SPOTIFY_ID;
-    let mysecret = process.env.SPOTIFY_SECRET;
+    let myid = keys.spotify.id;
+    let mysecret = keys.spotify.secret;
 
     let spotify = new Spotify({
         id: myid,
@@ -83,21 +88,44 @@ function spotify(variable) { //This code was supplied by node-spotify-api
     });
 }
 
+function twitter() {
+    console.log("Running Twitter Call");
+    let twitterKey = keys.twitter.consumer_key;
+    let twitterSecret = keys.twitter.consumer_secret;
+    let twitterAKey = keys.twitter.access_token_key;
+    let twitterASecret = keys.twitter.access_token_secret;
+
+    let client = new Twitter({
+        consumer_key: twitterKey,
+        consumer_secret: twitterSecret,
+        access_token_key: twitterAKey,
+        access_token_secret: twitterASecret
+      });
+
+    client.get('search/tweets', { q: 'node.js' }, function (error, tweets, response) {
+        for(a=0;a<tweets.statuses.length;a++){
+            let tweeted = tweets.statuses[a].text;
+            console.log(`* TWEET #${a} *********************************************************************************\nTweeted at: ${tweets.statuses[a].created_at}\nTweet: ${tweeted}\n* END OF TWEET #${a} **************************************************************************\n\n`);
+        }
+        // console.log(tweets)
+    });
+}
+
 function random() {
     fs.readFile("random.txt", "UTF8", function (err, data) {
         let ramChoices = data.split(", ")
-        // let availableChoice = ['movie-this','spotify-this-song','my-tweets']
+        // let availableChoice = ['movie-this','spotify-this-song','my-twitters']
         let command = Math.floor((Math.random() * 3)) // Random which three choice it picks (Rang: 0,1,2)
         let choice = Math.floor((Math.random() * ramChoices.length)) // Random a word from the random.txt
         // console.log("Action: " + availableChoice[command] + " Choice: " + ramChoices[choice])
         if (command == 0) { //IMDB
-            movie(ramChoices[choice])
+            movie(ramChoices[choice]);
         }
         else if (command == 1) { //Spotify
-            spotify(ramChoices[choice])
+            spotify(ramChoices[choice]);
         }
-        else if (command == 2) { //Tweeter
-            console.log("Tweeter Selected: Start Working on it!")
+        else if (command == 2) { //Twitter
+            twitter();
         }
     });
 }
